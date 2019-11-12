9580cc00a7a4c57cc865b3249fa3d1707fe7b02a
luoyanlin <cnskyrin@163.com>
2019-11-12 09:55:51 +0800

    update animation hero

--- lib/animations/hero_page.dart
+++ lib/animations/hero_page.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenge/animations/photo_hero.dart';
 
-class HeroPage extends StatefulWidget {
-  @override
-  _HeroPageState createState() => _HeroPageState();
-}
-
-class _HeroPageState extends State<HeroPage> {
+class HeroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    timeDilation = 5.0;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(runtimeType
-            .toString()
-            .substring(1, runtimeType.toString().length - "State".length)),
-      ),
-      body: Center()
+        appBar: AppBar(
+          title: Text("HeroPage"),
+        ),
+        body: Center(
+          child: PhotoHero(
+            photo: 'images/dzq.jpg',
+            width: 300.0,
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext ctx){
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text('FlippersPage'),
+                    ),
+                    body: Container(
+                      color: Colors.green,
+                      padding: EdgeInsets.all(16.0),
+                      alignment: Alignment.topLeft,
+                      child: PhotoHero(
+                        photo: 'images/dzq.jpg',
+                        width: 100,
+                        onTap: (){
+                          Navigator.of(ctx).pop();
+                        },
+                      ),
+                    ),
+                  );
+                }
+              ));
+            },
+          ),
+        )
     );
   }
 }
+
---
+++ lib/animations/photo_hero.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PhotoHero extends StatelessWidget {
+  const PhotoHero({Key key,this.photo,this.onTap,this.width});
+  final String photo;
+  final VoidCallback onTap;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: Hero(
+        tag: photo,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            child: Image.asset(
+              photo,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
